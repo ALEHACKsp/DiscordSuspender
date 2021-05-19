7aa214ecf0db8f7656b372f83d76aba753a64df0
InsideExploit <71732299+InsideExploit@users.noreply.github.com>
2021-05-19 12:01:15 +0200

    Not async function

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ token = "" # Account da far sospendere
 inv = "discord-testers" # Discord casuale
 
 
-async def ban_account(): # API Triggering
+def ban_account(): # API Triggering
     r = requests.post(f'https://discord.com/api/v9/invites/{inv}', headers={'Authorization': token})
     print(f'{Fore.GREEN}code: {r.status_code}')
 
